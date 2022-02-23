c8372266dfe5d6f8115e47acef9cdb2ef55279ba
William Drew <wdrew@governmentcio.com>
2022-02-23 12:33:19 -0500

    Using var.VPC_CIDR_BLOCK instead of hard coded value

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -3,8 +3,8 @@
 #---------------------------
 
 resource "aws_vpc" "main" {
-  #cidr_block           = "${var.VPC_CIDR_BLOCK}"
-  cidr_block           = "11.11.0.0/24"
+  cidr_block           = "${var.VPC_CIDR_BLOCK}"
+  #cidr_block           = "11.11.0.0/24"
   instance_tenancy     = "default"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
