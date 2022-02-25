80fcaf4c82612f3103da80248377cffc6de50615
William Drew <wdrew@governmentcio.com>
2022-02-25 15:31:52 -0500

    Changed enable_dns_support to false

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -5,7 +5,7 @@
 resource "aws_vpc" "main" {
   cidr_block           = "${var._cidr_block}"
   instance_tenancy     = "default"
-  enable_dns_support   = "true"
+  enable_dns_support   = "false"
   enable_dns_hostnames = "true"
   enable_classiclink   = "false"
 
